bb1a3c0d2f432e51ed0f0c72edb2dbc23e2a2b27
Julia Gomes <136478421+Juliagomes7@users.noreply.github.com>
2025-05-18 00:07:24 -0300

    Update BoxesAndDots.java

--- dotsNboxes/core/src/main/java/com/boxesNdots/BoxesAndDots.java
+++ dotsNboxes/core/src/main/java/com/boxesNdots/BoxesAndDots.java
@@ -1,40 +1,10 @@
 package com.boxesNdots;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
 
-public class BoxesAndDots extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private ShapeRenderer shapeRenderer;
-    private Bolinhas bolinhas;
-    private Linhas linhas;
-
+public class BoxesAndDots extends Game {
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        shapeRenderer = new ShapeRenderer();
-        bolinhas = new Bolinhas();
-        linhas = new Linhas(bolinhas.getPosicoesX(), bolinhas.getPosicoesY(), bolinhas.getTamanho());
-    }
-
-    @Override
-    public void render() {
-        ScreenUtils.clear(1f, 0.85f, 0.9f, 1f);
-
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        linhas.render(shapeRenderer);
-        bolinhas.render(shapeRenderer);
-        shapeRenderer.end();
-
-        batch.begin();
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        shapeRenderer.dispose();
-        batch.dispose();
+        setScreen(new SimpleMenuScreen(this));
     }
 }
