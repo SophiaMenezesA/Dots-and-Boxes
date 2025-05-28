ab6ed00ec36f8f15ead37e517d9b70d2f3828972
Kamila Pires <136478550+KamisP13@users.noreply.github.com>
2025-05-28 19:17:38 -0300

    Update GameOverScreen.java
    Finalizações

--- dotsNboxes/core/src/main/java/com/boxesNdots/GameOverScreen.java
+++ dotsNboxes/core/src/main/java/com/boxesNdots/GameOverScreen.java
@@ -3,6 +3,7 @@ package com.boxesNdots;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -15,6 +16,7 @@ public class GameOverScreen implements Screen {
     private SpriteBatch batch;
     private BitmapFont font;
     private boolean modoBot;
+    private Music musicaTelaFinal;
 
     public GameOverScreen(BoxesAndDots game, int vencedor, int pontosJogador1, int pontosJogador2, boolean modoBot) {
         this.game = game;
@@ -31,6 +33,10 @@ public class GameOverScreen implements Screen {
         font = new BitmapFont();
         font.getData().setScale(3);
         font.setColor(0.180f, 0.000f, 0.243f, 1);
+        musicaTelaFinal = Gdx.audio.newMusic(Gdx.files.internal("energetic-chiptune-video-game-music-platformer-8-bit-318348.mp3"));
+        musicaTelaFinal .play();
+        musicaTelaFinal .setVolume(0.5f);
+        musicaTelaFinal .setLooping(true);
     }
 
     @Override
@@ -55,17 +61,25 @@ public class GameOverScreen implements Screen {
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
             game.setScreen(new SimpleMenuScreen(game));
+            musicaTelaFinal.stop();
+            dispose();
         }
     }
 
     @Override public void resize(int width, int height) {}
     @Override public void pause() {}
     @Override public void resume() {}
-    @Override public void hide() {}
+    @Override 
+    public void hide() {
+        if (musicaTelaFinal != null) {
+            musicaTelaFinal.stop();
+        }
+    }
     
     @Override
     public void dispose() {
         batch.dispose();
         font.dispose();
+        if (musicaTelaFinal != null) musicaTelaFinal.dispose();
     }
 }
